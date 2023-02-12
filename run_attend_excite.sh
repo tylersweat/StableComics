86e7c1f9cd9e7fe873cbac4d76e49b2c23c0075c
Tyler Sweat <tysweat0@login04.rc.byu.edu>
2023-02-12 16:10:27 -0700

    Job submission script

---
+++ run_attend_excite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --time=00:10:00
+#SBATCH --mem=24G
+#SBATCH --gpus=1
+#SBATCH --nodes=1
+#SBATCH -C pascal
+
+
+cd ~/AttendExcite/
+
+nvidia-smi --list-gpus
+nvidia-smi --query-gpu=memory.total --format=csv
+
+python run_batch.py
